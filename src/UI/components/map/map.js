5641ec96349e361dc9ee1303cf20623d64843602
Суматохин А.С <as.sumatohin@corp.tensor.ru>
2019-07-09 16:00:47 +0300

    'first'

--- src/UI/components/map/map.js
+++ src/UI/components/map/map.js
@@ -3,9 +3,20 @@ import {connect} from 'react-redux';
 
 
 class DumpMap extends Component {
+    componentDidMount() {
+        ymaps.ready(this.init);
+    }
+
+    init() {
+        this.Ymap = new ymaps.Map("YandexMap", {
+            center: [55.76, 37.64],
+            zoom: 7
+        });
+    }
+
     render() {
         return (
-            <div id="YandexMaps"></div>
+            <div id="YandexMap" style={{"width": "600px", "height": "400px"}}></div>
         );
     }
 }
