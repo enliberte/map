383b4f608817bef1487a2551d8aafd721c39dae6
Суматохин А.С <as.sumatohin@corp.tensor.ru>
2019-07-12 17:58:30 +0300

    'cookie'

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
-const {apiRouter, getPlacemarks} = require('./api/api');
+const {apiRouter} = require('./api/api');
 const bodyParser = require('body-parser');
+const cookiesMiddleware = require('universal-cookie-express');
+
+
 const port = process.env.PORT || 8080;
 const app = express();
-
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/dist'));
+app.use(cookiesMiddleware());
 
 
 app.get('/', (req, res) => {
@@ -19,12 +22,8 @@ app.post('/', (req, res) => {
     const method = req.body.method;
     const params = req.body.params;
     const promise = apiRouter(method, params);
-    promise
-        .then(result => res.send(result.rows))
-        .catch(err => console.log(err));
+    promise.then(result => res.send(result.rows)).catch(err => res.status(404).send(err));
 });
-
-
 
 
 app.listen(port);
