09e9b192d047979d1318c648c596ad8fa3b7b121
Суматохин А.С <as.sumatohin@corp.tensor.ru>
2019-07-16 10:26:35 +0300

    'form'

--- client/src/BLL/store/store.js
+++ client/src/BLL/store/store.js
@@ -4,10 +4,16 @@ import placemarks from  './reducers/placemarks';
 import auth from './reducers/auth';
 import initialState from "./initialState";
 import authPanel from "./reducers/authPanel";
+import {reducer as formReducer} from 'redux-form';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(
-    combineReducers({placemarks, auth, authPanel}),
+    combineReducers({
+        placemarks,
+        auth,
+        authPanel,
+        form: formReducer
+    }),
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
