a7ad3f1f64cda426999c3cb7a9d737ace4131d88
enliberte <zugife21@gmail.com>
2019-07-17 22:06:28 +0300

    bugfix

--- server/api/sql/placemarks.js
+++ server/api/sql/placemarks.js
@@ -27,7 +27,7 @@ const insertPlacemark = (data) => {
         text: 'INSERT INTO placemarks (id, address, latitude, longitude, comment, construction, ' +
                                       'glass, household, metal, other, paper, plastic, violationType, state) ' +
               'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
-        values: [id, address, coords, comment, construction, glass, household, metal, other, paper, plastic, violationType, state]
+        values: [id, address, latitude, longitude, comment, construction, glass, household, metal, other, paper, plastic, violationType, state]
     };
 };
 
