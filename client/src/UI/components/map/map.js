448b3f2e085e3038598d8a2bc445c0f5a22e6e78
Суматохин А.С <as.sumatohin@corp.tensor.ru>
2019-07-18 09:18:34 +0300

    'refactoring'

--- client/src/UI/components/map/map.js
+++ client/src/UI/components/map/map.js
@@ -26,19 +26,8 @@ class DumpMap extends Component {
             });
 
             this.Ymap.events.add('click', (event) => this.props.onOpenCreateItemCard(event.get('coords')));
+            this.addNewPlacemarks(this.props.placemarks);
 
-            const placemarks = this.props.placemarks.map(placemark => new ymaps.Placemark(
-                [placemark.latitude, placemark.longitude],  {
-                    hintContent: placemark.address,
-                    balloonContent: placemark.state
-                }, {
-                    preset: 'islands#icon',
-                    iconColor: colors[placemark.state]
-                })
-            );
-            for (let placemark of placemarks) {
-                this.Ymap.geoObjects.add(placemark)
-            }
 
             // osme.geoJSON('RU-YAR', {lang: 'ru'}, (data) => {
             //     let collection = osme.toYandex(data, ymaps);
@@ -75,44 +64,56 @@ class DumpMap extends Component {
         );
     }
 
+    createPlacemark() {
+        this.newPlacemark = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: this.props.newPlacemark.coords
+            }
+        });
+        this.Ymap.geoObjects.add(this.newPlacemark);
+    }
+
+    addNewPlacemarks(placemarks) {
+        const placemarksObj = placemarks.map(placemark => new ymaps.Placemark(
+            [placemark.latitude, placemark.longitude],  {
+                hintContent: placemark.address,
+                balloonContent: placemark.state,
+                id: placemark.id
+            }, {
+                preset: 'islands#icon',
+                iconColor: colors[placemark.state]
+            })
+        );
+        for (let placemarkObj of placemarksObj) {
+            this.Ymap.geoObjects.add(placemarkObj);
+            placemarkObj.events.add('click', (event) => {
+                console.log(placemarkObj.get('id'));
+            });
+        }
+    }
+
+    removeNewPlacemark() {
+        if (this.newPlacemark) {
+            this.Ymap.geoObjects.remove(this.newPlacemark);
+        }
+    }
+
     componentDidUpdate(prevProps) {
         if (this.Ymap) {
             if (this.props.newPlacemark.isDisplayed) {
                 if (this.props.newPlacemark.coords !== prevProps.newPlacemark.coords) {
-                    if (this.newPlacemark) {
-                        this.Ymap.geoObjects.remove(this.newPlacemark);
-                    }
-                    this.newPlacemark = new ymaps.GeoObject({
-                        geometry: {
-                            type: "Point",
-                            coordinates: this.props.newPlacemark.coords
-                        }
-                    });
-                    this.Ymap.geoObjects.add(this.newPlacemark);
+                    this.removeNewPlacemark();
+                    this.createPlacemark();
                 }
             } else {
-                if (this.newPlacemark) {
-                    this.Ymap.geoObjects.remove(this.newPlacemark);
-                }
+                this.removeNewPlacemark()
             }
             if (this.props.placemarks !== prevProps.placemarks) {
-
                 const addedPlacemarks = this.props.placemarks.filter(
                     placemark => !prevProps.placemarks.some(oldPlacemark => oldPlacemark.id === placemark.id)
                 );
-                console.log(addedPlacemarks);
-                const placemarks = addedPlacemarks.map(placemark => new ymaps.Placemark(
-                    [placemark.latitude, placemark.longitude],  {
-                        hintContent: placemark.address,
-                        balloonContent: placemark.state
-                    }, {
-                        preset: 'islands#icon',
-                        iconColor: colors[placemark.state]
-                    })
-                );
-                for (let placemark of placemarks) {
-                    this.Ymap.geoObjects.add(placemark)
-                }
+                this.addNewPlacemarks(addedPlacemarks);
             }
         }
     }
