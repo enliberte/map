c04e609939569a68b8f311af9b5b03b99471ed55
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-08-08 16:11:47 +0300

    'saga'

--- client/src/BLL/store/saga/rootSaga.js
+++ client/src/BLL/store/saga/rootSaga.js
@@ -1,1 +1,20 @@
-const
+import {all} from 'redux-saga/effects';
+import {watchIsAuthorized, watchLogout, watchAuth, watchRegister} from './watchers/authWatchers';
+import {watchSetPlacemarks, watchSavePlacemark, watchUpdatePlacemark, watchDeletePlacemark, watchAddNewPlacemarkWithAddress, watchSetNewAddressAndCoords, watchEditCoords} from './watchers/placemarkWatchers';
+
+
+export default function *rootSaga() {
+	yield all([
+		watchIsAuthorized,
+		watchLogout,
+		watchAuth,
+		watchRegister,
+		watchSetPlacemarks,
+		watchSavePlacemark,
+		watchUpdatePlacemark,
+		watchDeletePlacemark,
+		watchAddNewPlacemarkWithAddress,
+		watchSetNewAddressAndCoords,
+		watchEditCoords
+	]);
+}
