bb0dceac897b8e20d53b8076a56bebc772b17112
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-08-09 11:02:44 +0300

    'selectors'

--- client/src/BLL/store/reducers/leftPanel.js
+++ client/src/BLL/store/reducers/leftPanel.js
@@ -1,42 +1,43 @@
 import {actions as a} from "../constants";
 
-const getStateWithActivePanel = (state, activePanel) => {
-    let newState = {};
-    for (let panel in state) {
-        newState[panel] = false;
-    }
-    newState[activePanel] = true;
-    return newState;
-}
+const initState = () => ({
+    authPanel: false,
+    regPanel: false,
+    createItemCard: false,
+    readItemCard: false,
+    editItemCard: false,
+    inWorkItemCard: false,
+    doneItemCard: false,
+})
 
 const leftPanel = (state={}, action) => {
     switch (action.type) {
         case a.OPEN_AUTH_PANEL:
-            return getStateWithActivePanel('authPanel');
+            return {...initState, authPanel: true};
         case a.CLOSE_AUTH_PANEL:
             return {...state, authPanel: false};
         case a.OPEN_REG_PANEL:
-            return getStateWithActivePanel('regPanel');
+            return {...initState, regPanel: true};
         case a.CLOSE_REG_PANEL:
             return {...state, regPanel: false};   
         case a.OPEN_CREATE_ITEM_CARD:
-            return getStateWithActivePanel('createItemCard');
+            return {...initState, createItemCard: true};
         case a.CLOSE_CREATE_ITEM_CARD:
             return {...state, createItemCard: false};
         case a.OPEN_READ_ITEM_CARD:
-            return getStateWithActivePanel('readItemCard');
+            return {...initState, readItemCard: true};
         case a.CLOSE_READ_ITEM_CARD:
             return {...state, readItemCard: false};   
         case a.OPEN_EDIT_ITEM_CARD:
-            return getStateWithActivePanel('editItemCard');
+            return {...state, editItemCard: true}; 
         case a.CLOSE_EDIT_ITEM_CARD:
             return {...state, editItemCard: false};  
         case a.OPEN_DONE_ITEM_CARD:
-            return getStateWithActivePanel('doneItemCard');
+            return {...state, doneItemCard: true}; 
         case a.CLOSE_DONE_ITEM_CARD:
             return {...state, doneItemCard: false};
         case a.OPEN_IN_WORK_ITEM_CARD:
-            return getStateWithActivePanel('inWorkItemCard');
+            return {...state, inWorkItemCard: true}; 
         case a.CLOSE_IN_WORK_ITEM_CARD:
             return {...state, inWorkItemCard: false};   
         default:
